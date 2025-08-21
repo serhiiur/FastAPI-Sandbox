e2249aada412f4b053c7f7dac2a1dc8788f6d285
prathamlahoti123 <prathamlahoti@duck.com>
2025-08-21 11:03:02 +0300

    Add usage of different thread pool executors

--- fastapi-threadpool-executor/api.py
+++ fastapi-threadpool-executor/api.py
@@ -1,40 +1,40 @@
 import asyncio
 import threading
 import time
+from asyncio.events import AbstractEventLoop
+from collections.abc import AsyncIterator
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import asynccontextmanager
-from typing import Annotated, AsyncIterator, TypedDict, TYPE_CHECKING
+from typing import Annotated, TypedDict
 
 from fastapi import Depends, FastAPI, Request
 from fastapi.concurrency import run_in_threadpool
 
-if TYPE_CHECKING:
-  from asyncio.unix_events import _UnixSelectorEventLoop
-
 
 class AppState(TypedDict):
-  """State of the main app"""
+  """State of the main app."""
+
   pool: ThreadPoolExecutor
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[AppState]:
-  """
-  Initialize a custom thread pool executor
-  in which we can run synchronous blocking function
-  without blocking the main thread.
+  """Init custom thread pool executor.
+
+  The custom executor is used to run synchronous
+  blocking functions without blocking the main thread.
   """
   with ThreadPoolExecutor() as pool:
     yield AppState(pool=pool)
 
 
 async def get_thread_pool_executor(request: Request) -> ThreadPoolExecutor:
-  """Return initialized thread pool executor object in the lifespan"""
-  return getattr(request.state, "pool")
+  """Return initialized thread pool executor object in the lifespan."""
+  return request.state.pool
 
 
-async def get_running_loop() -> "_UnixSelectorEventLoop":
-  """Return running loop"""
+async def get_running_loop() -> AbstractEventLoop:
+  """Return running loop."""
   return asyncio.get_running_loop()
 
 
@@ -43,13 +43,11 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/run-in-custom-executor")
 async def run_in_custom_executor(
-  loop: Annotated["_UnixSelectorEventLoop", Depends(get_running_loop)],
-  pool: Annotated[ThreadPoolExecutor, Depends(get_thread_pool_executor)]
+  loop: Annotated[AbstractEventLoop, Depends(get_running_loop)],
+  pool: Annotated[ThreadPoolExecutor, Depends(get_thread_pool_executor)],
 ) -> None:
-  """
-  Execute a blocking synchronous function (time.sleep)
-  in a separate thread using custom executor.
-  
+  """Execute a synchronous blocking function using custom thread pool executor.
+
   As a result, the main thread won't be blocked and users
   will be able to continue working with the API.
   """
@@ -58,19 +56,15 @@ async def run_in_custom_executor(
 
 @app.get("/run-in-starlette-executor")
 async def run_in_starlette_executor() -> None:
-  """
-  Execute a blocking synchronous function (time.sleep)
-  in using Starlette's 'run_in_threadpool' function.
-  
+  """Execute a synchronous blocking function using Starlette's thread pool executor.
+
   As a result, the main thread won't be blocked and users
   will be able to continue working with the API.
   """
   await run_in_threadpool(time.sleep, 30)
 
 
-@app.get(
-  "/threads",
-  description="Display the number of Thread objects currently alive"
-)
+@app.get("/threads")
 async def get_active_threads() -> int:
+  """Display the number of Thread objects currently alive."""
   return threading.active_count()
